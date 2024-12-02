244c980c6385f7eb42aa87ba2f06f3a84f238ed4
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-12-02 22:40:36 +0530

    add localstroge in todo

--- ToDo better virson/todo.js
+++ ToDo better virson/todo.js
@@ -1,14 +1,24 @@
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem('work')) || [];
 let btn = document.querySelector("#addTask");
 let inputTask = document.querySelector("#taskInput")
 btn.addEventListener("click",function(){
   let task = inputTask.value;
   tasks.push(task);
   inputTask.value = '';
+  localStorage.setItem('work',JSON.stringify(tasks));
+  window.location.reload();
+});
+
+for(task of tasks){
   let newTask = document.createElement("li")
   newTask.innerText = task;
   let ul = document.querySelector("ul")
   ul.insertAdjacentElement("afterbegin",newTask)
-});
-// localStorage.setItem("work", tasks);
-
+  let btn = document.createElement("button")
+  btn.innerText = "Delete task";
+  document.querySelector("li").insertAdjacentElement("beforeend",btn)
+  btn.addEventListener("click",function(){
+    
+    console.log("hey")
+  })
+}
