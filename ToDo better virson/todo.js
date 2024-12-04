869e5ef798a3173a71c0b50255a9f7846436d079
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-12-04 23:59:29 +0530

    done my project

--- ToDo better virson/todo.js
+++ ToDo better virson/todo.js
@@ -2,9 +2,30 @@ document.querySelector(".push").onclick = function(){
   if(document.querySelector(".newtask input").value.length == 0){
     alert("Plese enter a Task");
   }else {
-    console.log(document.querySelector(".newtask input").value)
+    document.querySelector(".tasks").innerHTML += `
+        <div class="task">
+          <span id="taskname">
+            ${document.querySelector('.newtask input').value}
+          </span>
+          <button class="detele">
+              <i class="fa-regular fa-trash-can"></i>
+          </button>
+        </div>
+    `;
     document.querySelector(".newtask input").value = '';
+    for(task of document.querySelectorAll(".task button")){
+      task.onclick = function(){
+        this.parentNode.remove();
+      }
+    }
+    for(task of document.querySelectorAll(".task #taskname")){
+      task.onclick = function(){
+        this.style.textDecoration = "line-through";
+      }
+    }
   }
-  }
+}
+
+
 
   
