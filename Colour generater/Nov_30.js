b548f1779357228ae1bfe9bdbbe838ff76f4c82e
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-12-05 23:32:39 +0530

    Add color generater to miniProjects

--- Colour generater/Nov_30.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("button").onclick = function(){
-  let c1 = Math.floor(Math.random() * 255);
-  let c2 = Math.floor(Math.random() * 255);
-  let c3 = Math.floor(Math.random() * 255);
-  let backgroundColor = `rgb(${c1},${c2},${c3})`;
-  document.querySelector("h1").innerText = backgroundColor;
-  document.querySelector(".colorBox").style.backgroundColor = backgroundColor;
-}
