bf709fbfe4a33da67a755812d99729ed181f5ba5
vikrant-vikrant <123059176+vikrant-vikrant@users.noreply.github.com>
2024-12-05 23:30:07 +0530

    improve a litter bit

--- Colour generater/Nov_30.js
+++ Colour generater/Nov_30.js
@@ -1,12 +1,8 @@
-let btn = document.querySelector("button");
-btn.onclick = function(){
+document.querySelector("button").onclick = function(){
   let c1 = Math.floor(Math.random() * 255);
   let c2 = Math.floor(Math.random() * 255);
   let c3 = Math.floor(Math.random() * 255);
-  let box = document.querySelector(".colorBox");
-  let backgroundColor = `(${c1},${c2},${c3})`;
-  box.style.backgroundColor = `rgb${backgroundColor}`;
-  box.style.border = `rgb${backgroundColor}`;
-  box.style.borderRadius = "5rem";
-  document.querySelector("h1").innerText = `rgb(${c1},${c2},${c3})`
+  let backgroundColor = `rgb(${c1},${c2},${c3})`;
+  document.querySelector("h1").innerText = backgroundColor;
+  document.querySelector(".colorBox").style.backgroundColor = backgroundColor;
 }
